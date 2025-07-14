c7176ad349d946885e4fd8ddbc7f20840e614d18
Navneet Pathak <navneetpathak1909@gmail.com>
2025-07-14 13:54:55 +0530

    Remove info

--- src/utils/constant.js
+++
@@ -1,7 +0,0 @@
-export const API_OPTIONS = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YWE3MTBmNzIwNGUyNWVlNGYxNzc1MmQ5MDU5OGNiYSIsIm5iZiI6MTc1MjQxNzczMS45OSwic3ViIjoiNjg3M2M1YzM4Yzg1ZTBlNWI5ZWQ3Zjg1Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.Q_I2dtzza_Y7UBy2HpMHs93JBUQEKPIdzqJz9ZE-ryI'
-  }
-};
